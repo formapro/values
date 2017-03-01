b5fd534f19a52c076d9e69716de4ffe8b476d737
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2017-03-01 20:48:04 +0200

    Remove commented out code.

--- src/ChangedValuesTrait.php
+++ src/ChangedValuesTrait.php
@@ -12,8 +12,6 @@ trait ChangedValuesTrait
     {
         $resetChangedValuesHook = function() {
             $this->changedValues = [];
-
-
         };
 
         $trackChangesHook = function($object, $key, $value, $modified) {
@@ -30,32 +28,8 @@ trait ChangedValuesTrait
             return $value;
         };
 
-//        $trackSetObjectChangesHook = function($context, $key, $object, $modified) {
-//            if ($object) {
-//                array_set($key, get_values($object), $this->changedValues);
-//            } else {
-//                if ($modified) {
-//                    array_set($key, null, $this->changedValues);
-//                }
-//            }
-//        };
-//
-//        $trackSetObjectsChangesHook = function($context, $key, $objects, $modified) {
-//            if ($objects) {
-//                foreach ($objects as $objectKey => $object) {
-//                    array_set($key.'.'.$objectKey, get_values($object), $this->changedValues);
-//                }
-//            } else {
-//                if ($modified) {
-//                    array_set($key, null, $this->changedValues);
-//                }
-//            }
-//        };
-
         register_hook($this, 'post_set_values', $resetChangedValuesHook);
         register_hook($this, 'post_set_value', $trackChangesHook);
         register_hook($this, 'post_add_value', $trackChangesHook);
-//        register_hook($this, 'post_set_object', $trackSetObjectChangesHook);
-//        register_hook($this, 'post_set_objects', $trackSetObjectsChangesHook);
     }
 }
