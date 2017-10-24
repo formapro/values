562dec9a676254c160409e4f93386c51ea94f6a2
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2017-10-24 12:28:43 +0300

    Object builder hook.

---
+++ src/ObjectBuilderHook.php
@@ -0,0 +1,39 @@
+<?php
+namespace Makasim\Values;
+
+class ObjectBuilderHook
+{
+    /**
+     * @var string[]
+     */
+    private $classMap;
+
+    /**
+     * @var string
+     */
+    private $fieldName;
+
+    /**
+     * @param string[] $classMap
+     * @param string $fieldName
+     */
+    public function __construct(array $classMap, string $fieldName = 'schema')
+    {
+        $this->classMap = $classMap;
+        $this->fieldName = $fieldName;
+    }
+
+    public function register()
+    {
+        register_global_hook('get_object_class', function(array $values) {
+            if (false == isset($values[$this->fieldName])) {
+                return;
+            }
+            if (false == array_key_exists($values[$this->fieldName], $this->classMap)) {
+                return;
+            }
+
+            return $this->classMap[$values[$this->fieldName]];
+        });
+    }
+}
